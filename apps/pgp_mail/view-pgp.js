89abec9b47d44ea3edf95ff7b08fc341298f7485
Julian Bäume <julian@svg4all.de>
2013-10-22 18:34:51 +0200

    filter pgp encryption related attachments
    those should not be shown to the user and are now filtered out. In most
    cases this will just filter all attachments from being shown.

--- apps/pgp_mail/view-pgp.js
+++ apps/pgp_mail/view-pgp.js
@@ -3,7 +3,26 @@ define('pgp_mail/view-pgp',
         'io.ox/mail/api'
     ], function (ext, api) {
 
+
     'use strict';
+
+    ext.point('io.ox/mail/detail/header').extend({
+        before: 'attachments',
+        id: 'filter_attachments',
+        draw: function (baton) {
+            if (!baton.data.attachment || baton.data.content_type !== 'multipart/encrypted') {
+                return;
+            }
+            var filtered = baton.data.attachments.filter(function (attachment) {
+                return (/^application\/pgp-encrypted$/).test(attachment.content_type) ||
+                       (/^application\/octet-stream/).test(attachment.content_type);
+            });
+
+            baton.data.attachment = false;
+            baton.data.attachments = _(baton.data.attachments).difference(filtered);
+            baton.data.pgp_attachments = filtered;
+        }
+    });
 
     ext.point('io.ox/mail/detail').extend({
         index: 400,
@@ -13,11 +32,12 @@ define('pgp_mail/view-pgp',
             if (baton.data.content_type !== 'multipart/encrypted') {
                 return;
             }
-            var data = _(baton.data.attachments).find(function (a) {
+            var data = _(baton.data.pgp_attachments).find(function (a) {
                     return (/^application\/octet-stream/).test(a.content_type);
                 }),
                 node = this.find('.content');
 
+            data.mail = baton.data;
             $.ajax({ url: api.getUrl(data, 'view'), dataType: 'text' }).done(function (text) {
                 node.empty().append(
                     $('<pre class="encrypted">').html(text)
