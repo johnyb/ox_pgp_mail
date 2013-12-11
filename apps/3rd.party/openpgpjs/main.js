d28c9d0c0d6c4a541a657dc2993ac4df14a7ef3a
Julian Bäume <julian@svg4all.de>
2013-12-11 18:40:51 +0100

    implement openpgps showMessages with notifications
    this adds some nice feedback for the user, consistent with other appsuite ui warnings

--- apps/3rd.party/openpgpjs/main.js
+++ apps/3rd.party/openpgpjs/main.js
@@ -1,6 +1,13 @@
 define('3rd.party/openpgpjs/main', [
+    'io.ox/core/notifications',
     '3rd.party/openpgpjs/openpgp'
-], function () {
+], function (notifications) {
+
+    window.showMessages = function (text) {
+        var txt = $(text);
+        txt = txt.text().replace(/^WARNING:/, '');
+        notifications.yell('warning', txt);
+    };
     /* global openpgp */
     return openpgp;
 });
