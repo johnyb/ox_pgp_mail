c66f6f01ad606a5b1018e4df9f2d70b920e8e0af
Julian Bäume <julian@svg4all.de>
2014-07-22 16:34:51 +0200

    add first view extensions tests
    very simple, for now … but one gets the idea

--- spec/pgp_mail/view_spec.js
+++ spec/pgp_mail/view_spec.js
@@ -1,6 +1,19 @@
-define(['pgp_mail/view-pgp'], function() {
+define([
+    'io.ox/core/extensions',
+    'pgp_mail/view-pgp'
+], function (ext) {
     'use strict';
 
     describe('PGP Mail Views', function () {
+        describe('should extend io.ox/mail/detail/body', function () {
+            var point = ext.point('io.ox/mail/detail/body');
+            it('for encrypted mails', function () {
+                expect(point.keys()).to.contain('encrypted_content');
+            });
+
+            it('for signed mails', function () {
+                expect(point.keys()).to.contain('signed_content');
+            });
+        });
     });
 });
