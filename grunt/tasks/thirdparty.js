752c6666ba166d0041db039cf39c924f5d6275a1
Julian Bäume <julian@svg4all.de>
2015-09-17 14:37:24 +0200

    integrate mailbuild dependency
    needed to build MIME blocks to send via OX HTTP API.

---
+++ grunt/tasks/thirdparty.js
@@ -0,0 +1,36 @@
+module.exports = function (grunt) {
+
+    grunt.config.merge({ concat: {
+        thirdparty: {
+            options: {
+                banner: 'var root = {};\ndefine("static/3rd.party/mailbuild.js", function () {\n\n' +
+                        '"use strict";\n\n' +
+                        'var define = undefined;\n' +
+                        'var exports = undefined;\n' +
+                        'var global = undefined;\n' +
+                        '//this is bound to the "root" variable from above\n' +
+                        'this.TextEncoder = window.TextEncoder;\n' +
+                        'this.TextDecoder = window.TextDecoder;\n' +
+                        'this.btoa = window.btoa;\n',
+                footer: 'return this;\n}.bind(root));',
+                process: function (src, file) {
+                    //FIXME: remove this function, once mailbuild@>0.3.6 is released (see https://github.com/whiteout-io/mailbuild/pull/15)
+                    if (file !== 'node_modules/mailbuild/src/mailbuild.js') return src;
+
+                    return src.replace(/root.mailbuild = factory\(mimefuncs, mimetypes, punycode, addressparser\);/,
+                                       'root.mailbuild = factory(root.mimefuncs, root.mimetypes, root.punycode, root.addressparser);');
+                }
+            },
+            files: [{
+                src: [
+                    'node_modules/mailbuild/node_modules/mimefuncs/src/mimefuncs.js',
+                    'node_modules/mailbuild/node_modules/mimetypes/src/mimetypes.js',
+                    'node_modules/mailbuild/node_modules/punycode/punycode.js',
+                    'node_modules/mailbuild/node_modules/wo-addressparser/src/addressparser.js',
+                    'node_modules/mailbuild/src/mailbuild.js'
+                ],
+                dest: 'build/static/3rd.party/mailbuild.js'
+            }]
+        }
+    }});
+};
